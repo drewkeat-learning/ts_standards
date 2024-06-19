9c7807cb1e15b10173ec5f888aee1cc382183b49
drewkeat <drewjkeat@gmail.com>
2024-06-19 12:17:28 -0600

    Before testing Prisma/Supabase

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-export {};
-//compare dbjurs w/ oljurs
-//if dbjur not cached fetch jurstandardsets
-//for each standardset, fetch standards
-//for each standard write standard to db
-//write standard as cached
-//after mark standardset as cached
-//after all standardset is fetched/cached mark jur as cached
-//# sourceMappingURL=index.js.map
